cd89c3561d018036d25358a15ef1c81e6efff55d
Fabrizio <fabrizio.carlassara@gmail.com>
2024-03-20 18:55:04 +0000

    docs: se comentó el código

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from gpiozero import LED, Button
 from signal import pause
-
+#importo las librerias que usere (led, botón y pausa)
 led = LED(13)
+#armo una variable para el led de la placa
 button = Button(18)
-
+#armo una variable para el botón
 button.when_pressed = led.on
+#uso la funcion when_pressed para cambiar el estado del LED a ON cuando 
+#se presiona el botón
 button.when_released = led.off
+#uso la funcion when_released para cambiar el estado del led a OFF cuando
+#se suelta el botón
 
-pause()
