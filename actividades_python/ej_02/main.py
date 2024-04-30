3b5dbdf41f6f50783dcdd49b82ae56ad0b412688
CabreraMartinAV41 <85530194+CabreraMartinAV41@users.noreply.github.com>
2024-04-30 10:23:35 -0300

    "docs: comenté el codigo"

--- actividades_python/ej_02/main.py
+++ actividades_python/ej_02/main.py
@@ -1,27 +1,26 @@
 from gpiozero import LED, Button
-from time import sleep
 from signal import pause
+from time import sleep
 
-#declaro los distintos leds de acuerdo a sus colores
+#declaro que led usar y su respectivo color
 led_verde = LED(13)
 led_rojo = LED(19)
 led_azul = LED(26)
 
-#bucle que enciende y apaga los leds
+#declaro un bucle while true que enciende los leds un tiempo determinado
+#y los apaga
 while True:
 	led_rojo.on()
 	sleep(1)
 	led_rojo.off()
-	#led rojo encendido durante 1 segundo, luego se apaga
-
+	
 	led_azul.on()
 	sleep(0.5)
 	led_azul.off()
-	#led azul encendido, mantengo ese estado por medio segundo y apago
 
 	led_verde.on()
 	sleep(0.25)
 	led_verde.off()
-	#Led verde encendido por un cuarto de segundo y luego se apaga
+	
 
 pause()
