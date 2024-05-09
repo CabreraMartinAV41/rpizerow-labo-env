2ff01450af60ed0b3d41bd8b8a9545690083ba9c
CabreraMartinAV41 <85530194+CabreraMartinAV41@users.noreply.github.com>
2024-05-09 15:59:42 -0300

    Delete actividadesRyP/ej_01 directory

--- actividadesRyP/ej_01/client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-HOST = socket.gethostbyname(socket.gethostname())                # Symbolic name meaning all available interfaces
-PORT = 50007              # Arbitrary non-privileged port
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-    print(data)
