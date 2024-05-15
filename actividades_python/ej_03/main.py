5f2ff8c78a9977fa5ae71f62fa3e3928a81fbd43
CabreraMartinAV41 <85530194+CabreraMartinAV41@users.noreply.github.com>
2024-05-15 16:39:22 -0300

    Update main.py

--- actividades_python/ej_03/main.py
+++ actividades_python/ej_03/main.py
@@ -17,7 +17,6 @@ while True:
     elif res == "buzz.off":
         bzz.off()
     elif res == "rgb.red":
-<<<<<<< HEAD
       
         led_rojo.on()
     elif res == "rgb.blue":
@@ -27,9 +26,7 @@ while True:
         
         led_verde.on()
         
-=======
         led_azul.off()
-        led_verde.off()
         led_rojo.on()
     elif res == "rgb.blue":
         led_azul.on()
@@ -39,7 +36,7 @@ while True:
         led_azul.off()
         led_verde.on()
         led_rojo.off()
->>>>>>> 3648ab40d0a49a8c79a954b782497d3460700c9b
+
     else:
         # Manejo de errores para comandos incorrectos
         print("Error: no se reconoció el comando")
